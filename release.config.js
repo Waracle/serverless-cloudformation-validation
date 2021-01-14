8bb9bb97642bcdb92f01cd95f77bffc7ec4ef58a
Nick Hammond <n.hammond@waracle.com>
2021-01-14 16:30:16 +0000

    fix(build): Ensure changelog is generated
    Signed-off-by: Nick Hammond <n.hammond@waracle.com>

--- release.config.js
+++ release.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    // installed by default
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
-    // extra plugins here
-    '@semantic-release/changelog',
+    '@semantic-release/git',
   ],
 };
