8035f57ea369b3ca6d0defb1d829f2f10b1b1812
Nick Hammond <n.hammond@waracle.com>
2021-01-14 12:45:54 +0000

    chore: Add testing config

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  collectCoverage: false,
+  coverageReporters: [
+    "json-summary",
+    "lcov",
+    "text"
+  ],
+  moduleFileExtensions: [
+    "ts",
+    "tsx",
+    "js",
+    "json"
+  ],
+  preset: "ts-jest",
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        output: "./test-reports/junit.xml"
+      }
+    ]
+  ],
+  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  testEnvironment: "node",
+  verbose: true
+};
