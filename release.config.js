ead11326e15f7289e1dbe7a9212d64af5c34203f
Nick Hammond <n.hammond@waracle.com>
2021-01-14 12:46:40 +0000

    chore: Add semantic releasing config

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    // installed by default
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    // extra plugins here
+    "@semantic-release/changelog"
+  ]
+};
