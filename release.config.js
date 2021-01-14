c2776de1bc8e0946dbc07fe444ccc3d2d1cb56ff
Nick Hammond <n.hammond@waracle.com>
2021-01-14 16:15:19 +0000

    build: Format release config

--- release.config.js
+++ release.config.js
@@ -2,11 +2,11 @@ module.exports = {
   branches: ['main'],
   plugins: [
     // installed by default
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
     // extra plugins here
-    "@semantic-release/changelog"
-  ]
+    '@semantic-release/changelog',
+  ],
 };
